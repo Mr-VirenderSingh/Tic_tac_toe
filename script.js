84ad6ca8de6679dd5bda818c218314534c9a6bc4
Vireder <virendersd621@gmail.com>
2024-01-13 17:44:36 +0530

    minnor changes

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const btns = document.querySelectorAll(".btn");
+let btns = document.querySelectorAll(".btn");
 
-let playerTurn = 0;
-btns.forEach((btn) => {
-  btn.addEventListener("click", (turn) => {
-    if(playerTurn === 0){
-      btn.innerHTML = "X";
-      playerTurn = 1;
-    }else{
-      btn.innerHTML = "O";
-      playerTurn = 0;
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], // horizontal
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8], // vertical
+  [0, 4, 8],
+  [2, 4, 6], // diagonal
+];
+
+let turn = 0;
+btns.forEach((btn) =>
+  btn.addEventListener("click", () => {
+    if (btn.innerHTML === "") {
+      if (turn === 0) {
+        btn.innerHTML = "X";
+        turn = 1;
+        checkWinner();
+      } else {
+        btn.innerHTML = "O";
+        turn = 0;
+        checkWinner();
+      }
     }
-  });
-});
+  })
+);
+
+function checkWinner() {
+  for (let pattens of winConditions) {
+    let val1 = btns[pattens[0]].innerText;
+    let val2 = btns[pattens[1]].innerText;
+    let val3 = btns[pattens[2]].innerText;
+    if (val1 !== "" && val2 !== "" && val3 !== "") {
+      if (val1 && val1 === val2 && val2 === val3) {
+        btns.disabled = true;
+      }
+    }
+  }
+}
